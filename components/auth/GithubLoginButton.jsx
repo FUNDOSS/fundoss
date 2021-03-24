78a9e4811651c759ddf0295100113a3f2957534c
Francois Richir <francois@humanific.com>
2021-03-24 12:34:11 +0700

    sizing and variants

--- components/auth/GithubLoginButton.jsx
+++ components/auth/GithubLoginButton.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Icons from '../icons';
 
-const GithubLoginButton = ({ redirect }) => (
-  <Button variant="primary" href={`/api/auth/github${redirect ? `?redirect=${redirect}` : ''}`}>
-    <Icons.Github size={22} />
+const GithubLoginButton = ({ redirect, variant, size }) => (
+  <Button variant={variant || 'primary'} size={size || 'md'} href={`/api/auth/github${redirect ? `?redirect=${redirect}` : ''}`}>
+    <Icons.Github size={{ sm: 18, lg: 25, md: 20 }[size]} />
     &nbsp;Sign in with GitHub
   </Button>
 );
