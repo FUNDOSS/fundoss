e826a33337a012aab6082380e96f02e423dc5141
Francois Richir <francois@humanific.com>
2021-03-31 11:51:51 +0700

    add multiple items to cart

--- pages/share/[sid].jsx
+++ pages/share/[sid].jsx
@@ -10,61 +10,48 @@ import Payments from '../../lib/payment/paymentController';
 import CartController from '../../lib/cart/CartController';
 import FundingSessions from '../../lib/fundingSession/fundingSessionController'; 
 import Qf from '../../utils/qf';
+import AddMultipleToCartButton from '../../components/cart/AddMultipleToCartButton'
 import { formatAmountForDisplay } from '../../utils/currency';
-import Cart from '../../components/cart/Cart';
 
-const SharePage = ({ user, payment, session, cart }) => (
+const SharePage = ({ user, payment, session, cart }) => {
+  
+  return (
   <Layout title="FundOSS | Donations cart" user={user} cart={cart} session={session} style={{background: '#0E0C4D'}}>
-    <div className="confetti" style={{ marginBottom: '-60px' , paddingBottom: '70px'}}>
-      <Container>
-        <Card style={{ maxWidth: '750px', margin: '30px auto' }}>
-          <Card.Body className="text-center">
-            <Image src={payment.user.avatar} roundedCircle width={100} />
-            <h3>
-              {payment.user.name} has backed {Pluralize('awesome collective', payment.donations.length, true)}
-            </h3>
-            <p className="lead">And unlocked <span className="text-fat text-success">{
-            formatAmountForDisplay(payment.donations.reduce(
-              (total, donation) => total += Qf.calculate(donation.amount),
-              0
-            ))
-            }</span> in estimated matched funding</p>
-            <Row className="justify-content-md-center" style={{ maxWidth: '550px', margin: '10px auto' }}>
-              {payment.donations.map(
-                (donation) => (
-                  <Col xs={2}  key={donation._id}>
-                    <Image src={donation.collective.imageUrl} roundedCircle fluid />
-                  </Col>
-                ),
-              )}
-            </Row>
-            <Button 
-              variant="outline-primary" 
-              size="lg"
-              onClick={()=>
-                {Cart.addItems(payment.donations.map(donation => ({
-                  collective:donation.collective, 
-                  amount:20
-                })))
-              }}
-            >Add {Pluralize('collective', payment.donations.length, true)} to my cart
-            
-            </Button><br />
-            And unlock <span className="text-fat text-success">{
-            formatAmountForDisplay(payment.donations.reduce(
-              (total) => total += Qf.calculate(20),
-              0
-            ))
-            }</span> in estimated matches
-          </Card.Body>
-          <Card.Footer>
-            <Button size="lg" variant="primary" block href="/">More collectives on FundOSS.org</Button>
-          </Card.Footer>
-        </Card>
-      </Container>
-    </div>
+              <Container style={{ paddingTop: '40px', margin: '-60px 0' }} fluid>
+              <Row className="no-gutter align-items-center">
+                <Col md={6} className="illu-hand d-none d-md-block" style={{ minHeight: '750px' }}>
+
+                </Col>
+                <Col md={{ span: 4, offset:1 }} className="text-center">
+                <div style={{ maxWidth: '550px', margin:'100px auto' }}>
+                  <Image src={payment.user.avatar} roundedCircle width={100} />
+                  <h2 style={{margin:'20px 0'}}>
+                    {payment.user.name} has backed<br /> {Pluralize('awesome collective', payment.donations.length, true)}
+                  </h2>
+                  <p className="lead">And unlocked <span className="text-fat text-success">{
+                  formatAmountForDisplay(payment.donations.reduce(
+                    (total, donation) => total += Qf.calculate(donation.amount),
+                    0
+                  ))
+                  }</span> in estimated matched funding</p>
+                  <Row className="justify-content-md-center" style={{ maxWidth: '550px', margin: '10px auto' }}>
+                    {payment.donations.map(
+                      (donation) => (
+                        <Col xs={4}  key={donation._id}><a className="black text-fat" href={'/collective/'+donation.collective.slug}>
+                          <Image src={donation.collective.imageUrl} roundedCircle fluid width={45} /><br />
+                          {donation.collective.name}
+                          </a></Col>
+                      ),
+                    )}
+                  </Row>
+                  <AddMultipleToCartButton items={payment.donations.map(d => d.collective)}/> 
+                  <Button variant="outline-primary" size="lg" block href="/">More collectives on FundOSS.org</Button>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
   </Layout>
-);
+)};
 
 export async function getServerSideProps({ query, req, res }) {
   await middleware.run(req, res);
