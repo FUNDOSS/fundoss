4c28acc06217a4fcab1c9163ffe4ab3e4d728991
Francois Richir <francois@humanific.com>
2021-03-31 11:51:22 +0700

    submit issues

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,6 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Logo from './Logo';
 import ShareButton from '../components/social/ShareButton';
+import { Button } from 'react-bootstrap';
 
 const Footer = ({ minimal }) => (
   <footer>
@@ -29,7 +30,8 @@ const Footer = ({ minimal }) => (
               <Nav.Link>OSC Slack</Nav.Link>
             </Nav>
           </Col>
-          <Col>
+          <Col> 
+            
             <h5>Join</h5>
             <Nav className="flex-column">
               <Nav.Link>Register to Donate</Nav.Link>
@@ -40,6 +42,7 @@ const Footer = ({ minimal }) => (
             <p>Fund OSS is a partnership between <b>Gitcoin</b> and <b>Open Source Collective</b></p>
           </Col>
           <Col md={6}>
+          <Button href="https://github.com/humanific/fundoss/issues" block size="lg" variant="outline-light">🐞Submit Bugs or Issues</Button>
           <h5>Share FundOSS.org</h5>
           <ShareButton platform="twitter" variant="link" url={'/'} />
           <ShareButton platform="facebook" variant="link" url={'/'} />
