fca4f52c6b40436a2a8446b8822b316902deec08
Francois Richir <francois@humanific.com>
2021-04-05 10:20:09 +0700

    graphing fixes

--- components/qf/calculator.jsx
+++ components/qf/calculator.jsx
@@ -1,22 +1,19 @@
-import React , { useState } from 'react';
+import React, { useState } from 'react';
 import { Form, Row, Col } from 'react-bootstrap';
 import Graph from './graph';
 import Qf from '../../utils/qf';
 
-
-const Calculator = () => {
-  const [form , setForm] = useState( {
-      matchedFunds:75000,
-      averageDonationEst:20,
-      numberDonationEst:2000,
-    }
-    );
+const Calculator = ({ exp, fudge, inout }) => {
+  const [form, setForm] = useState({
+    matchedFunds: 75000,
+    averageDonationEst: 20,
+    numberDonationEst: 2000,
+  });
 
   return (
     <Form>
-    <Row>
+      <Row>
         <Col>
-        
           <Form.Group>
             <Form.Label>Matched Funds</Form.Label>
             <Form.Control
@@ -26,7 +23,7 @@ const Calculator = () => {
               step={1000}
               min={1000}
               value={form.matchedFunds}
-              onChange={(e) => setForm({...form, ...{matchedFunds: e.currentTarget.value} })}
+              onChange={(e) => setForm({ ...form, ...{ matchedFunds: e.currentTarget.value } })}
             />
           </Form.Group>
         </Col>
@@ -39,7 +36,9 @@ const Calculator = () => {
               placeholder="in USD"
               min={1}
               value={form.averageDonationEst}
-              onChange={(e) => setForm({...form, ...{averageDonationEst: e.currentTarget.value} })}
+              onChange={(e) => setForm(
+                { ...form, ...{ averageDonationEst: e.currentTarget.value } },
+              )}
             />
           </Form.Group>
         </Col>
@@ -53,24 +52,31 @@ const Calculator = () => {
               placeholder="in USD"
               min={100}
               value={form.numberDonationEst}
-              onChange={(e) => setForm({...form, ...{numberDonationEst: e.currentTarget.value} })}
+              onChange={(e) => setForm(
+                { ...form, ...{ numberDonationEst: e.currentTarget.value } },
+              )}
             />
           </Form.Group>
         </Col>
       </Row>
       <Graph 
         plot={(x) => Qf.calculate(
-            x, 
-            form.averageDonationEst, 
-            form.matchedFunds / form.numberDonationEst
-          )} 
+          x, 
+          form.averageDonationEst, 
+          form.matchedFunds / form.numberDonationEst,
+          exp, 
+          fudge, 
+          inout,
+        )} 
+        fudge={fudge} 
+        exp={exp}
         averageDonation={Math.round(form.averageDonationEst)} 
         averageMatch={Math.round(form.matchedFunds / form.numberDonationEst)} 
         width={550}
         height={200}
-      /></Form>
-)
+      />
+    </Form>
+  );
 };
-
 
 export default Calculator;
--- components/qf/graph.jsx
+++ components/qf/graph.jsx
@@ -1,49 +1,66 @@
-import * as React from "react"
+import React from 'react';
 
-function Graph({plot, averageDonation, averageMatch, width = 800, height = 600 }) {
+function Graph({
+  plot, averageDonation, averageMatch, width = 800, height = 600, fudge, exp,
+}) {
   const plotGraph = () => {
-    const data = []
-    for(let i=0; i<width; i+=5) data.push({x:i, y:Math.round(plot(i))})
-    return data.reduce((path, {x, y}) => path += ' L' + (x+10) +' '+ ( height - y - 10 ), 'M 10 '+ (height - 10));
-  }
+    const data = [];
+    for (let i = 0; i < width; i += 2) data.push({ x: i, y: Math.round(plot(i)) });
+    return data.reduce((path, { x, y }) => `${path} L${x + 10} ${height - y - 10}`, `M 10 ${height - 10}`);
+  };
   return (
     <svg
       width={width}
       height={height}
-      viewBox={'0 0 '+ width+' '+ height}
+      viewBox={`0 0 ${width} ${height}`}
       xmlns="http://www.w3.org/2000/svg"
       
     >
       <defs>
-        <marker id="arrowhead" markerWidth="10" markerHeight="7" 
-        refX="0" refY="3.5" orient="auto">
-          <polygon points="0 0, 5 3.5, 0 7"  fill="#6c757d"/>
+        <marker
+          id="arrowhead"
+          markerWidth="10"
+          markerHeight="7" 
+          refX="0"
+          refY="3.5"
+          orient="auto"
+        >
+          <polygon points="0 0, 5 3.5, 0 7" fill="#6c757d" />
         </marker>
-        <marker id="startarrow" markerWidth="10" markerHeight="7" 
-        refX="5" refY="3.5" orient="auto">
-          <polygon points="5 0, 5 7, 0 3.5" fill="#6c757d"/>
+        <marker
+          id="startarrow"
+          markerWidth="10"
+          markerHeight="7" 
+          refX="5"
+          refY="3.5"
+          orient="auto"
+        >
+          <polygon points="5 0, 5 7, 0 3.5" fill="#6c757d" />
         </marker>
       </defs>
-      <title>{"Matching Formula"}</title>
-      <path d={'M 10 10 V '+ (height-10) +' H '+(width-10)} stroke="#6c757d" fill="transparent" markerEnd="url(#arrowhead)" markerStart="url(#startarrow)" /> 
+      <title>Matching Formula</title>
+      <path d={`M 10 10 V ${height - 10} H ${width - 10}`} stroke="#6c757d" fill="transparent" markerEnd="url(#arrowhead)" markerStart="url(#startarrow)" /> 
       
-      <path d={'M '+ (10 + averageDonation) + ' 10 V '+ (height-10)} stroke="#20c997" />
-      <path d={'M 10 '+ ((height-10) - averageMatch) + ' H '+(width-10)} stroke="#E76127" />
-      <text x="20" y="40" style={{fontSize:'12px'}}>
+      <path d={`M ${10 + averageDonation} 10 V ${height - 10}`} stroke="#20c997" />
+      <path d={`M 10 ${(height - 10) - averageMatch} H ${width - 10}`} stroke="#E76127" />
+      <text x="20" y="40" style={{ fontSize: '12px' }}>
         match $
       </text>
-      <text x={width-200} y={height-20} style={{fontSize:'12px'}}>
+      <text x="100" y="40" style={{ fontSize: '12px' }}>
+        fudge: {fudge || 1} exp:{exp || 2}
+      </text>
+      <text x={width - 200} y={height - 20} style={{ fontSize: '12px' }}>
         donation $
       </text>
-      <text x={(20 + averageDonation)} y="70" style={{fill:'#20c997',fontSize:'12px'}}>
+      <text x={(20 + averageDonation)} y="70" style={{ fill: '#20c997', fontSize: '12px' }}>
         average donation ${averageDonation}
       </text>
-      <text x={width - 200} y={ height - averageMatch - 20} style={{fill:'#E76127',fontSize:'12px'}}>
-        average match ${Math.round(averageMatch*100)/100}
+      <text x={width - 200} y={height - averageMatch - 20} style={{ fill: '#E76127', fontSize: '12px' }}>
+        average match ${Math.round(averageMatch * 100) / 100}
       </text>
       <path d={plotGraph()} stroke="#9451EB" fill="transparent" strokeWidth="2" strokeLinecap="round" />
     </svg>
-  )
+  );
 }
  
-export default Graph
+export default Graph;
