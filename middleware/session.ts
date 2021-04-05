af4d83c8e0b4f03e5ca049350cdd047a1c70f858
Francois Richir <francois@humanific.com>
2021-04-05 13:51:30 +0700

    proxy pass

--- middleware/session.ts
+++ middleware/session.ts
@@ -14,12 +14,13 @@ export default async function sessionMiddleware(req, res, next) {
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
+    proxy: false,
     store: mongoStore,
     cookie: { secure: false },
   };
   if (process.env.HOSTING_URL.indexOf('https') === 0) {
+    sessionConfig.proxy = true;
     sessionConfig.cookie.secure = true;
   }
-  console.log('sessionConfig.cookie.secure', sessionConfig.cookie.secure);
   return session(sessionConfig)(req, res, next);
 }
