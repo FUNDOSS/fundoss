5abc373551b8510a04b9d1f867de8035e080f97a
Francois Richir <francois@humanific.com>
2021-04-05 10:15:36 +0700

    cleanup

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,9 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
-import Logo from './Logo';
-import ShareButton from '../components/social/ShareButton';
 import { Button } from 'react-bootstrap';
+import Logo from './Logo';
+import ShareButton from './social/ShareButton';
 
 const Footer = ({ minimal }) => (
   <footer>
@@ -19,36 +19,41 @@ const Footer = ({ minimal }) => (
             <h5>Platform</h5>
             <Nav className="flex-column">
               <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/faq">FAQ</Nav.Link>
+              <Nav.Link href="https://blog.opencollective.com/fundoss-faqv1/">FAQ</Nav.Link>
               <Nav.Link href="/quadratic-funding">How democratic funding works</Nav.Link>
             </Nav>
             <h5>Social</h5>
             <Nav className="flex-column">
-              <Nav.Link>Twitter</Nav.Link>
-              <Nav.Link>Facebook</Nav.Link>
-              <Nav.Link>Gitcoin Slack</Nav.Link>
-              <Nav.Link>OSC Slack</Nav.Link>
+              <Nav.Link href="https://twitter.com/opencollect" target="_blank">OSC Twitter</Nav.Link>
+              <Nav.Link href="https://twitter.com/gitcoin" target="_blank">Gitcoin Twitter</Nav.Link>
+              <Nav.Link href="https://discord.com/invite/jWUzf7b8Yr" target="_blank">Gitcoin Discord</Nav.Link>
+              <Nav.Link href="https://slack.opencollective.com/" target="_blank">OSC Slack</Nav.Link>
             </Nav>
           </Col>
           <Col> 
             
             <h5>Join</h5>
             <Nav className="flex-column">
-              <Nav.Link>Register to Donate</Nav.Link>
-              <Nav.Link>Create A Collective</Nav.Link>
-              <Nav.Link>Get In Touch</Nav.Link>
+              <Nav.Link href="/api/auth/github">Register to Donate</Nav.Link>
+              <Nav.Link href="https://opencollective.com/create" target="_blank">Create A Collective</Nav.Link>
+              <Nav.Link href="mailto:info@opencollective.com">Get In Touch</Nav.Link>
             </Nav>
             <h5>Partnership</h5>
-            <p>Fund OSS is a partnership between <b>Gitcoin</b> and <b>Open Source Collective</b></p>
+            <p>
+              Fund OSS is a partnership between <b>Gitcoin</b>&nbsp;
+              and <b>Open Source Collective</b>
+            </p>
           </Col>
           <Col md={6}>
-          <Button href="https://github.com/humanific/fundoss/issues" block size="lg" variant="outline-light">🐞Submit Bugs or Issues</Button>
-          <h5>Share FundOSS.org</h5>
-          <ShareButton platform="twitter" variant="link" url={'/'} />
-          <ShareButton platform="facebook" variant="link" url={'/'} />
-          <ShareButton platform="email" variant="link" url={'/'} />
-          <p>Another way you can suppor the amazing OSS projects hosted here is by getting the word out!</p>
-          <p>Please consider sharing with your friends and family.</p>
+            <Button href="https://github.com/humanific/fundoss/issues" block size="lg" variant="outline-light">🐞Submit Bugs or Issues</Button>
+            <h5>Share FundOSS.org</h5>
+            <ShareButton platform="twitter" variant="link" url="/" />
+            <ShareButton platform="facebook" variant="link" url="/" />
+            <ShareButton platform="email" variant="link" url="/" />
+            <p>Another way you can suppor the amazing OSS&nbsp;
+              projects hosted here is by getting the word out!
+            </p>
+            <p>Please consider sharing with your friends and family.</p>
           </Col>
         </Row>
       </Container>
--- components/layout.jsx
+++ components/layout.jsx
@@ -10,42 +10,47 @@ import Footer from './Footer';
 import Icons from './icons';
 import Qf from '../utils/qf';
 
-
 const Layout = ({
-  children, title = 'This is the default title', user, hidefooter, cart, session
+  children, title = 'This is the default title', user, hidefooter, cart, predicted,
 }) => {
-
-  if(session) Qf.init(session.averageDonationEst, session.matchedFunds/session.numberDonationEst);
-  if(user) Cart.previousDonations = user.donations;
+  if (predicted) {
+    Qf.init(
+      predicted.donation, 
+      predicted.match,
+      predicted.fudge,
+    ); 
+  }
+  if (user) Cart.previousDonations = user.donations;
 
   return (
-  <div id="main" style={{display:'none'}}>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <Navbar bg="light" expand="lg" fixed="top">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Brand href="/"><Logo /></Navbar.Brand>
-        <Navbar.Collapse id="primary-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/about"><Icons.Code size={20} /> About</Nav.Link>
-            <Nav.Link href="/faq"><Icons.Question size={20} /> FAQ</Nav.Link>
-            <Nav.Link href="/quadratic-funding"><Icons.Buoy size={20} /> How democratic funding works</Nav.Link>
+    <div id="main" style={{ display: 'none' }}>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <Navbar bg="light" expand="lg" fixed="top">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Brand href="/"><Logo /></Navbar.Brand>
+          <Navbar.Collapse id="primary-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/about"><Icons.Code size={20} /> About</Nav.Link>
+              <Nav.Link href="https://blog.opencollective.com/fundoss-faqv1/"><Icons.Question size={20} /> FAQ</Nav.Link>
+              <Nav.Link href="/quadratic-funding"><Icons.Buoy size={20} /> How democratic funding works</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          { cart ? <CartButton className="btn-cart" itemCount={cart?.length} /> : null }
+          <Nav className="auth">
+            <AuthLinks user={user} />
           </Nav>
-        </Navbar.Collapse>
-        { cart ? <CartButton className="btn-cart" itemCount={cart?.length} /> : null }
-        <Nav className="auth">
-          <AuthLinks user={user} />
-        </Nav>
-      </Navbar>
-    </header>
-    {children}
-    <Footer minimal={hidefooter} />
-    { cart ? <Cart cart={cart} user={user} /> : null }
-  </div>
-)};
+        </Navbar>
+      </header>
+      {children}
+      <Footer minimal={hidefooter} />
+      { cart ? <Cart cart={cart} user={user} /> : null }
+    </div>
+  ); 
+};
 
 export default Layout;
