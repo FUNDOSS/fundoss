a96b92b6a6eee13163433ff7d60446f6d684f685
Francois Richir <francois@humanific.com>
2021-04-24 12:51:56 +0700

    pages + 404

--- pages/page/[slug].jsx
+++ pages/page/[slug].jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import moment from 'moment';
 import Container from 'react-bootstrap/Container';
 import { Row, Col } from 'react-bootstrap';
 import Ghost from '../../lib/ghost';
@@ -8,31 +7,30 @@ import middleware from '../../middleware/all';
 import ServerProps from '../../lib/serverProps';
 import Error from '../../components/Error';
 
-const PostsPage = ({ state, post }) => {
-  if (!post) {
+const GhostPage = ({ state, page }) => {
+  if (!page) {
     return <Error statusCode={404} />;
   }
   return (
-    <Layout title="FundOSS | Blog" state={state}>
+    <Layout title={`FundOSS | ${page.title}`} state={state}>
       <Container className="page-content">
         <Row>
           <Col className="post-content" md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
-            <div className="post" key={post.id}><h1>{post.title}</h1>
-              <div className="meta">{moment(post.published_at).format('MMMM Do YYYY')} </div>
-              <div className="gh-content" dangerouslySetInnerHTML={{ __html: post.html }} />
+            <div className="post" key={page.id}><h1>{page.title}</h1>
+              <div className="gh-content" dangerouslySetInnerHTML={{ __html: page.html }} />
             </div>
           </Col>
         </Row>
       </Container>
     </Layout>
-  );
+  ); 
 };
 
 export async function getServerSideProps({ query, req, res }) {
   await middleware.run(req, res);
   const state = await ServerProps.getAppState(req.user, req.session.cart);
-  const post = await Ghost.getPost(query.slug);
-  return { props: { state, post } };
+  const page = await Ghost.getPage(query.slug);
+  return { props: { state, page } };
 }
 
-export default PostsPage;
+export default GhostPage;
