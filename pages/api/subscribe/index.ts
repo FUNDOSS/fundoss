f3614c3f96a2ccf0242c7c3992d48780644ea8a7
Francois Richir <francois@humanific.com>
2021-04-28 07:59:20 +0700

    not authenticated bug

--- pages/api/subscribe/index.ts
+++ pages/api/subscribe/index.ts
@@ -11,10 +11,9 @@ handler.use(all);
 handler.post(async (req: any, res: NextApiResponse) => {
   if (req.body.email) {
     const sub = await Ghost.subscribe(req.body).catch((e) => console.log(e));
-    if (req.user._id) {
+    if (req.user?._id) {
       Users.update({ _id: req.user._id, subscribed: sub.subscribed });
     }
-    console.log(sub);
     return res.status(200).json({ sub });
   }
   return res.status(500).json({ statusCode: 500, message: 'email not sent' });
