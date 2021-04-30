e9ccc0fb18445eb1c41a96afe3b6803828d5c268
Francois Richir <francois@humanific.com>
2021-04-30 12:23:32 +0700

    amount graph update

--- components/cart/DonationInput.jsx
+++ components/cart/DonationInput.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { InputGroup, Form } from 'react-bootstrap';
 
 const DonationInput = ({ amount, min, max, onChange }) => {
   const [value, setValue] = useState(amount);
-
+  useEffect(() => {
+    if (value !== amount) {
+      setValue(amount);
+    }
+  }, [amount]);
   return (
     <InputGroup className="cart-amount" style={{ maxWidth: '150px', margin: '5px auto' }}>
       <InputGroup.Prepend><InputGroup.Text>$</InputGroup.Text></InputGroup.Prepend>
