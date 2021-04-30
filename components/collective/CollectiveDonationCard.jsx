ed9dbe7e43cc5aaeeb1035d8dcbc08557548374d
Francois Richir <francois@humanific.com>
2021-04-30 12:24:03 +0700

    cleanup

--- components/collective/CollectiveDonationCard.jsx
+++ components/collective/CollectiveDonationCard.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Pluralize from 'pluralize';
 import {
-  Badge, Col, Row, InputGroup, 
+  Badge, Col, Row,  
 } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Link from 'next/link';
