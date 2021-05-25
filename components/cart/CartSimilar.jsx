cd55f1e1602949bf984e237c8f9ac630e3c28597
Francois Richir <francois@humanific.com>
2021-05-25 16:16:22 +0700

    similar mobile

--- components/cart/CartSimilar.jsx
+++ components/cart/CartSimilar.jsx
@@ -1,35 +1,42 @@
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 import {
-  Card, Image, Row, Button, Col,
+  Card, Image, Row, Button, Col, Badge,
 } from 'react-bootstrap';
-import Icons from '../icons';
 
 const truncate = (input, length) => (input && input?.length > length ? `${input.substring(0, length)}...` : input);
 
 const CartSimilarCollective = ({ collective, addItem }) => (
-  <Card
-    className="text-center small"
-    style={{
-      height: '250px', marginBottom: '10px',  
-    }}
-  >
-    <Card.Header>
+  <>
+    <Card
+      className="text-center small d-none d-md-block"
+      style={{
+        height: '250px', marginBottom: '10px',  
+      }}
+    >
+      <Card.Header>
+        { collective?.imageUrl ? <Image width="30" height="30" src={collective.imageUrl} roundedCircle fluid /> : null }
+        <br />
+        <b>
+          <Link href={`/collective/${collective.slug}`}>{truncate(collective.name, 20)}</Link>
+        </b>
+      </Card.Header>
+      <Card.Body>
+        {truncate(collective.description, 30)}
+      </Card.Body>
+      <Card.Footer>
+        <Button block size="sm" variant="outline-primary" onClick={() => addItem(collective)}>
+          Add to cart
+        </Button>
+      </Card.Footer>
+    </Card>
+    <Button className="d-md-none d-block" block size="sm" variant="link" onClick={() => addItem(collective)}>
       { collective?.imageUrl ? <Image width="30" height="30" src={collective.imageUrl} roundedCircle fluid /> : null }
+      <Badge variant="info round">+</Badge>
       <br />
-      <b>
-        <Link href={`/collective/${collective.slug}`}>{truncate(collective.name, 20)}</Link>
-      </b>
-    </Card.Header>
-    <Card.Body>
-      {truncate(collective.description, 30)}
-    </Card.Body>
-    <Card.Footer>
-      <Button block size="sm" variant="outline-primary" onClick={() => addItem(collective)}>
-        Add to cart
-      </Button>
-    </Card.Footer>
-  </Card>
+      <b>{truncate(collective.name, 20)}</b>
+    </Button>
+  </>
 );
 
 const CartSimilar = ({ data, addItem }) => {
@@ -60,7 +67,6 @@ const CartSimilar = ({ data, addItem }) => {
         </Row>
       </div>
     ) : null 
-
   );
 };
 
