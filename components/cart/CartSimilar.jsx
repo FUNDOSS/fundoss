15910d6b7616d4e15fd000f6edd8d860aeca0c6d
Francois Richir <francois@humanific.com>
2021-05-26 11:43:56 +0700

    ui fine tuning

--- components/cart/CartSimilar.jsx
+++ components/cart/CartSimilar.jsx
@@ -9,30 +9,33 @@ const truncate = (input, length) => (input && input?.length > length ? `${input.
 const CartSimilarCollective = ({ collective, addItem }) => (
   <>
     <Card
+      style={{ width: '100%' }}
       className="text-center small d-none d-md-block"
-      style={{
-        height: '250px', marginBottom: '10px',  
-      }}
     >
-      <Card.Header>
+      <Card.Header style={{ padding: '10px' }}>
         { collective?.imageUrl ? <Image width="30" height="30" src={collective.imageUrl} roundedCircle fluid /> : null }
         <br />
-        <b>
+        <b className="text-fat">
           <Link href={`/collective/${collective.slug}`}>{truncate(collective.name, 20)}</Link>
         </b>
       </Card.Header>
-      <Card.Body>
+      <Card.Body style={{ padding: '10px' }}>
         {truncate(collective.description, 30)}
       </Card.Body>
-      <Card.Footer>
+      <Card.Footer style={{ padding: '10px' }}>
         <Button block size="sm" variant="outline-primary" onClick={() => addItem(collective)}>
           Add to cart
         </Button>
+        <Link href={`/collective/${collective.slug}`}>
+          <Button block size="sm" variant="link">
+            Read more
+          </Button>
+        </Link>
       </Card.Footer>
     </Card>
     <Button className="d-md-none d-block" block size="sm" variant="link" onClick={() => addItem(collective)}>
       { collective?.imageUrl ? <Image width="30" height="30" src={collective.imageUrl} roundedCircle fluid /> : null }
-      <Badge variant="info round">+</Badge>
+      <Badge style={{ margin: '-10px 0 10px 0' }} variant="info round">+</Badge>
       <br />
       <b>{truncate(collective.name, 20)}</b>
     </Button>
@@ -58,7 +61,7 @@ const CartSimilar = ({ data, addItem }) => {
           {
            similar.map(
              (collective) => (
-               <Col key={collective._id}>
+               <Col key={collective._id} className="d-flex align-items-stretch">
                  <CartSimilarCollective collective={collective} addItem={addItem} />
                </Col>
              ),
