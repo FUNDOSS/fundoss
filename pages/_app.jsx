8291faf1974c70b436307c3420d8f7fad6c3494c
Francois Richir <francois@humanific.com>
2021-06-05 18:01:04 +0700

    analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import '../styles/global.scss';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import NextNprogress from 'nextjs-progressbar';
+import '../styles/global.scss';
+import * as gtag from '../lib/gtag';
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div>
       <NextNprogress
