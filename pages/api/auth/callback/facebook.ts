287fb732420e3fc2f1912ce57ed7b4b536a521e3
Francois Richir <francois@humanific.com>
2021-06-14 16:28:30 +0700

    cleanup

--- pages/api/auth/callback/facebook.ts
+++ pages/api/auth/callback/facebook.ts
@@ -1,7 +1,7 @@
 import nextConnect from 'next-connect';
 import { NextApiResponse } from 'next';
 import { all } from '../../../../middleware/index';
-import passport from '../../../../middleware/passportfacebook';
+import passport from '../../../../middleware/passportFacebook';
 
 const handler = nextConnect();
 
@@ -10,12 +10,10 @@ handler.use(all);
 handler.get((req: any, res: NextApiResponse) => {
   const { state } = req.query;
   const redirect = Buffer.from(state as string, 'base64').toString();
-  console.log('resp cb', redirect);
 
   return passport.authenticate('facebook', {
     failureRedirect: '/',
   })(req, res, () => {
-    console.log('resp next', redirect);
     req.session.user = { user: req.user._id };
     return res.redirect(redirect);
   });
--- pages/api/auth/callback/google.ts
+++ pages/api/auth/callback/google.ts
@@ -10,12 +10,10 @@ handler.use(all);
 handler.get((req: any, res: NextApiResponse) => {
   const { state } = req.query;
   const redirect = Buffer.from(state as string, 'base64').toString();
-  console.log('resp cb', redirect);
 
   return passport.authenticate('google', {
     failureRedirect: '/',
   })(req, res, () => {
-    console.log('resp next', redirect);
     req.session.user = { user: req.user._id };
     return res.redirect(redirect);
   });
